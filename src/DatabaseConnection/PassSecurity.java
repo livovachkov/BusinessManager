83c672bf9eb1440858763bdae157a7f83e4070fd
livovachkov <livovachkov@gmail.com>
2020-11-25 13:10:36 -0800

    Minor 2

--- src/DatabaseConnection/PassSecurity.java
+++ src/DatabaseConnection/PassSecurity.java
@@ -8,36 +8,72 @@ package DatabaseConnection;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PassSecurity {
 
-    private static String convertToHex(byte[] data) { 
+    private static String convertToHex(byte[] data) {
         StringBuffer buf = new StringBuffer();
-        for (int i = 0; i < data.length; i++) { 
+        for (int i = 0; i < data.length; i++) {
             int halfbyte = (data[i] >>> 4) & 0x0F;
             int two_halfs = 0;
-            do { 
-                if ((0 <= halfbyte) && (halfbyte <= 9)) 
+            do {
+                if ((0 <= halfbyte) && (halfbyte <= 9)) {
                     buf.append((char) ('0' + halfbyte));
-                else 
+                } else {
                     buf.append((char) ('a' + (halfbyte - 10)));
+                }
                 halfbyte = data[i] & 0x0F;
-            } while(two_halfs++ < 1);
-        } 
+            } while (two_halfs++ < 1);
+        }
         return buf.toString();
     }
 
-
-    public static String getSHA1(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException  { 
+    public static String getSHA1(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         MessageDigest md;
         md = MessageDigest.getInstance("SHA-1");
         byte[] sha1hash = new byte[40];
         md.update(text.getBytes("iso-8859-1"), 0, text.length());
         sha1hash = md.digest();
         return convertToHex(sha1hash);
-    } 
+    }
 
+    private String generateActualSql(String sqlQuery, Object... parameters) {
+        String[] parts = sqlQuery.split("\\?");
+        StringBuilder sb = new StringBuilder();
+
+        // This might be wrong if some '?' are used as litteral '?'
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i];
+            sb.append(part);
+            if (i < parameters.length) {
+                sb.append(formatParameter(parameters[i]));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String formatParameter(Object parameter) {
+        if (parameter == null) {
+            return "NULL";
+        } else {
+            if (parameter instanceof String) {
+                return "'" + ((String) parameter).replace("'", "''") + "'";
+            } else if (parameter instanceof Timestamp) {
+                return "to_timestamp('" + new SimpleDateFormat("MM/dd/yyyy HH:mm:ss.SSS").
+                        format(parameter) + "', 'mm/dd/yyyy hh24:mi:ss.ff3')";
+            } else if (parameter instanceof Date) {
+                return "to_date('" + new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").
+                        format(parameter) + "', 'mm/dd/yyyy hh24:mi:ss')";
+            } else if (parameter instanceof Boolean) {
+                return ((Boolean) parameter).booleanValue() ? "1" : "0";
+            } else {
+                return parameter.toString();
+            }
+        }
+    }
 
 }
-
